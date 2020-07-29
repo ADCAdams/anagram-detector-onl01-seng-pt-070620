db0c269a8427add5321c9cf21d741fcc5c83d361
Andy Adams <andrewdcadams@gmail.com>
2020-07-29 00:57:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,13 +4,13 @@ class Anagram
   attr_accessor :word 
   
   def initialize(new_word)
-    @word = new_word.join
+    @word = new_word.split("")
   end 
   
   
   def match(word_array)
     word_array.each do |wordString|
-      single_word_array = wordString.join
+      single_word_array = wordString.split("")
       return wordString if single_word_array.sort == @word.sort
       
     end
